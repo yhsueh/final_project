1e2546d1db60c261ed3bf43c2ba99ad7a98c62e9
yhsueh <joehsueh@hotmail.com>
2017-11-28 12:11:28 -0500

    configure opencv libraries inside the package cmakelist.txt. also create a base cpp that will be used as the core component of the project. Right now, the base cpp is written to read the RGB image from the camera node published by Kinetic

---
+++ src/base.cpp
@@ -0,0 +1,31 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
+    cv::waitKey(30);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "base");
+  ros::NodeHandle nh;
+  cv::namedWindow("view");
+  cv::startWindowThread();
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("camera/rgb/image_raw", 1, imageCallback);
+  ros::spin();
+  cv::destroyWindow("view");
+}
