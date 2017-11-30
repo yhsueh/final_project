6f94b1125afbb2d231293229dd7fe306368f0d56
yhsueh <joehsueh@hotmail.com>
2017-11-29 22:31:09 -0500

    removing redundant classes

--- src/BaseTestCallback.cpp
+++
@@ -1,8 +0,0 @@
-#include <ros/ros.h>
-#include "BaseTestCallback.hpp"
-#include "std_msgs/Float32.h"
-#include <iostream>
-void BaseTestCallback::distanceCallback(const std_msgs::Float32::ConstPtr& msg) {
-	minimum = msg->data;
-	//std::cout << "Min: " << minimum << std::endl;
-}
