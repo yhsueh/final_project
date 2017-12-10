2d5c2328f05b1796c3be0fde531f5a4d99443724
yhsueh <joehsueh@hotmail.com>
2017-12-10 17:35:43 -0500

    Commit before trying new yml file

--- test/imageProcess_test.cpp
+++ test/imageProcess_test.cpp
@@ -1,2 +1,11 @@
 #include <gtest/gtest.h>
-#include <ImageProcess.h>
+#include "ImageProcess.hpp"
+
+
+
+
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
