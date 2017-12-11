3c91ddf6c1e9cad2217bb33cf7a1f9b2b1c67f04
yhsueh <joehsueh@hotmail.com>
2017-12-11 04:07:06 -0500

    Complete TskT0. TskT4 in progress.

--- test/imageProcess_test.cpp
+++ test/imageProcess_test.cpp
@@ -1,7 +1,55 @@
 #include <gtest/gtest.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include "ImageProcess.hpp"
 
+TEST(UnitTesting, color_test_blue_green) {
+	//ImageProcess imgObj;
+	//ImageProcess imgObj2;
 
+	cv::Mat image = cv::imread("../Blueball.jpg",CV_LOAD_IMAGE_COLOR);
+	//image2 = cv::imread("~/catkin_ws/src/final_package/Greenball.jpg", CV_LOAD_IMAGE_UNCHANGED);
+	//imgObj.loadImage(image);
+	int row = image.rows;
+
+	EXPECT_EQ(row,1);
+
+
+	//cv::cvtColor(image,image2,CV_BGR2HSV);
+	//imgObj.detection();
+	//imgObj2.loadImage(image2);
+
+	/*int color = 1;
+	int color2 = 1;
+	while(1) {
+		imgObj.color = color;
+		imgObj.detection();
+
+		if (imgObj.circles.size() > 0) {
+			break;
+		}
+
+		color+=1;
+		break;
+	}
+*/
+	//EXPECT_EQ(color,3);
+
+/*
+	while(1) {
+		imgObj2.color = color2;
+		imgObj2.detection();
+
+		if (imgObj2.circles.size() > 0) {
+			break;
+		}
+
+		color2 += 1;
+	}
+
+	EXPECT_EQ(color,2);
+	*/
+}
 
 
 
